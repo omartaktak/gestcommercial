314247a726c4cc52b8ad5357717044c6f7b7b1bf
OMAR <omar.taktak@gmail.com>
2016-02-15 11:38:00 +0100

    Second COmmit

--- src/main/java/tn/taktak/GestCommerciale_V1/repository/CompteurPieceRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.taktak.GestCommerciale_V1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tn.taktak.GestCommerciale_V1.entity.CompteurPiece;
-
-public interface CompteurPieceRepository extends JpaRepository<CompteurPiece, String> {
-
-}
--- src/main/java/tn/taktak/GestCommerciale_V1/repository/SocieteRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.taktak.GestCommerciale_V1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tn.taktak.GestCommerciale_V1.entity.Societe;
-
-public interface SocieteRepository extends JpaRepository<Societe, String> {
-
-}
